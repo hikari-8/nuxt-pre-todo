d268f25dd6822da76a7178d280a3d69bf150c60a
hikari <kanbarusuruga0127@gmail.com>
2022-08-16 13:39:31 +0900

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="app_container">
+        <div class="message">{{ message }}</div>
+    </div>
+</template>
+<script lang="ts">
+import { Vue, Component } from "nuxt-property-decorator";
+
+@Component({})
+export default class TopPage extends Vue {
+    public message = "HELLO WORLD, welcome to STAGE2";
+}
+</script>
+<style lang="stylus" scoped>
+.app_container {
+    min-height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    .message {
+        font-size: 40px;
+        text-align: center;
+        font-weight: bold;
+    }
+}
+</style>
